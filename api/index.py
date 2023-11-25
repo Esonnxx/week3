1107d8c6187f8d235469c856d772b7d81a2c4341
Esonnxx <77544102+Esonnxx@users.noreply.github.com>
2023-11-25 22:35:21 +0800

    fix some

--- api/index.py
+++ api/index.py
@@ -1,67 +1,90 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-from linebot.models.events import FollowEvent, MessageEvent, TextMessage
-import os
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-# 設定問題與答案的字典
 questions = {
-    1: "你今年幾歲",
-    2: "你的性別",
-    3: "你的名字",
-    4: "謝謝！問題結束。"
+    1: "避免你與人界接觸過少，請你列出可以聽你說話的名單，給自己在喪失陽氣前留一條退路。",
+    2: {
+        "text": "你傷心的時候，前任怎麼安慰你？",
+        "options": [
+            "擁抱",
+            "傳訊息說等他忙完後去找你",
+            "他說一切都會變好",
+            "會主動做些事說你小題大作",
+            "他直接消失"
+        ]
+    },
+    3: "你會做些甚麼避免自己憂傷沉淪？",
+    4: "🗝如果的是"
 }
 answers = {}
-# domain root
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route("/webhook", methods=['POST'])
+def create_flex_message(question_number):
+    question_data = questions.get(question_number)
+    if isinstance(question_data, dict):
+        return FlexSendMessage(
+            alt_text=question_data["text"],
+            contents={
+                "type": "bubble",
+                "direction": "ltr",
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                        {"type": "text", "text": question_data["text"], "weight": "bold", "size": "xl"},
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "margin": "lg",
+                            "spacing": "sm",
+                            "contents": [
+                                {"type": "text", "text": option, "wrap": True, "color": "#666666", "size": "md", "flex": 0} for option in question_data["options"]
+                            ]
+                        }
+                    ]
+                }
+            }
+        )
+    else:
+        return None
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
-        line_handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-@line_handler.add(FollowEvent)
-@line_handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
     user_message = event.message.text
 
-    current_question = len(answers) 
+    current_question = len(answers)
 
     if current_question <= len(questions):
         answers[current_question] = user_message
-        next_question = questions.get(current_question + 1)
+        flex_message = create_flex_message(current_question + 1)
 
-        if next_question:
+        if flex_message:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=next_question)
+                flex_message
             )
         else:
-            # 所有問題都已回答，可以在這裡進行其他操作
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="謝謝！問題結束。")
+                TextSendMessage(text="🗝如果的是")
             )
     else:
-        # 多餘的回答或其他處理方式
         pass
 
 if __name__ == "__main__":
