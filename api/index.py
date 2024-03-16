7d23adb4a78108b2fbe9ff9c18839d1cb2c5c7af
Esonnxx <eason10252043@gmail.com>
2024-03-16 23:45:36 +0800

    set gpt to false as default

--- api/index.py
+++ api/index.py
@@ -13,7 +13,6 @@ CarouselColumn,
 ConfirmTemplate,
 FlexSendMessage,
 ImageCarouselColumn, URITemplateAction)
-
 from linebot.models.events import FollowEvent, MessageEvent, TextMessage
 from api.chatgpt import ChatGPT
 import os
@@ -21,8 +20,11 @@ import os
 app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "false").lower() == "false"
+working_status = False
+
+
 chatgpt = ChatGPT()
+
 
 # domain root
 @app.route('/')
@@ -245,9 +247,12 @@ def handle_follow(event):
 
 
 def handle_message(event):
-    global working_status
+    global working_status 
+    
     if event.message.type != "text":
         return
+    
+    
     
     if event.message.text == "跟我聊聊":
         working_status = True
